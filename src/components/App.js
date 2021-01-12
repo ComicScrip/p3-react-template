ae1c6923e7d0c3177a2450188a7ad5cfb7754047
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-01-12 09:22:04 +0100

    get collection with filters sorting and pagination

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,23 @@
 import './App.css';
 
+import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import ContactList from './ContactList';
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <div className="App">Wild P3</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Link to="/contacts">Contacts</Link>
+        <Switch>
+          <Route exact path="/contacts" component={ContactList} />
+        </Switch>
+      </Router>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
